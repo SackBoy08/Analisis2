0029ec903807c0d73be2a0d8d5f52104b933e35b
SackBoy08 <sackby09@gmail.com>
2024-09-06 22:07:32 -0600

    Sprint_inicio
    En esta carpeta se encuentra todo lo necesario para el sitio web Bits&Bytes

---
+++ bits&bytes/includes/sidebar.php
@@ -0,0 +1,11 @@
+<div class="profile_nav">
+          <ul>
+            <li><a href="profile.php">Configuración de perfil</a></li>
+              <li><a href="update-password.php">Actualiza contraseña</a></li>
+            <li><a href="my-booking.php">Mis reservas</a></li>
+            <li><a href="post-testimonial.php">Publicar un comentario</a></li>
+               <li><a href="my-testimonials.php">Mis comentarios</a></li>
+            <li><a href="logout.php">Salir</a></li>
+          </ul>
+        </div>
+      </div>
